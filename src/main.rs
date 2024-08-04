a7853a6250a0ec001cb06f6d52a9d5d4b2c35471
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2024-08-04 00:39:06 -0700

    cleanup logging, add custom date input

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use base64::{prelude::BASE64_STANDARD, Engine};
+use chrono::NaiveDate;
 use chrono_tz::Tz;
 use clap::Parser;
 use regex::Regex;
 use scraper::{ElementRef, Html, Selector};
 
 use std::collections::HashMap;
-use std::io::Write;
+use std::path::PathBuf;
 
 // New releases happen at midnight US-West time
 const US_WEST_TZ: Tz = chrono_tz::America::Los_Angeles;
 
 const URL_PREFIX: &str = "aHR0cHM6Ly93d3cubnl0aW1lcy5jb20=";
 const URL_SUFFIX: &str = "Y3Jvc3N3b3Jkcy9zcGVsbGluZy1iZWUtZm9ydW0uaHRtbA==";
+
+const DEFAULT_FORMAT: &str = "./%Y-%m-%d-_ITEM_.csv";
 
 lazy_static::lazy_static! {
     static ref TABLE_SELECTOR: Selector = Selector::parse("table.table").unwrap();
@@ -27,7 +30,29 @@ lazy_static::lazy_static! {
 
 #[derive(clap::Parser, Debug)]
 struct Args {
+    /// The date to retrieve data for.
+    /// Format: YYYY-MM-DD
     date: Option<String>,
+
+    #[arg(short, long)]
+    /// The format of the filename to write files to.
+    /// _ITEM_ will be replaced with "pairs" or "lengths".
+    filename_format: Option<String>,
+}
+
+fn prepare_csv_path(d: &NaiveDate, template: &str, key: &str) -> PathBuf {
+    let csv_name = d.format(&template.replace("_ITEM_", key)).to_string();
+    let csv_path = PathBuf::from(&csv_name);
+    let dirname = csv_path.parent().unwrap();
+    if !dirname.exists() {
+        // TODO: return Result<>
+        std::fs::create_dir_all(dirname).expect("failed to make dir");
+    }
+
+    dirname
+        .canonicalize()
+        .expect("failed to canonicalize path")
+        .join(csv_path.file_name().unwrap())
 }
 
 #[tokio::main]
@@ -42,8 +67,6 @@ async fn main() {
     let suffix = String::from_utf8_lossy(&STR_URL_SUFFIX);
     let date_str = today.format("%Y/%m/%d");
     let url_str = format!("{prefix}/{date_str}/{suffix}");
-
-    eprintln!("{url_str}");
 
     // TODO: subtle user agent?
     let resp = reqwest::get(url_str)
@@ -68,9 +91,10 @@ async fn main() {
     let pairs = extract_pair_info(two_letters_el);
     let table_info = extract_table_info(table);
 
-    eprintln!("lengths:");
-    let csv_name = today.format("./%Y-%m-%d-lengths.csv").to_string();
-    let mut writer = csv::Writer::from_path(csv_name).expect("failed to open file");
+    // NOTE: can't use an unwrap_or here due to lifetime requirements
+    let template = args.filename_format.as_deref().unwrap_or(DEFAULT_FORMAT);
+    let lengths_path = prepare_csv_path(&today, template, "lengths");
+    let mut writer = csv::Writer::from_path(&lengths_path).expect("failed to open file");
 
     for ((letter, len), quantity) in table_info.iter() {
         // NOTE: csv writer expects these to be representable as &[u8], even if
@@ -82,19 +106,25 @@ async fn main() {
             .expect("failed to write length record");
     }
 
-    let mut stdout = std::io::stdout();
-    writeln!(&mut stdout).expect("failed to write empty line");
-    // let mut writer = csv::Writer::from_writer(stdout);
-    // let mut
-    let csv_name = today.format("./%Y-%m-%d-pairs.csv").to_string();
-    let mut writer = csv::Writer::from_path(csv_name).expect("failed to open pairs file");
-    eprintln!("pairs:");
+    let pairs_path = prepare_csv_path(&today, template, "pairs");
+    let mut writer = csv::Writer::from_path(&pairs_path).expect("failed to open pairs file");
     for ((a, b), v) in pairs.iter() {
         let record = [format!("{a}{b}"), v.to_string()];
         writer
             .write_record(record)
             .expect("failed to write pair record");
     }
+
+    eprintln!("operation success!");
+    eprintln!("pairs written to:   {}", pairs_path.to_string_lossy());
+    eprintln!("lengths written to: {}", lengths_path.to_string_lossy());
+
+    eprintln!();
+    eprintln!("instructions:\n---");
+
+    eprintln!("import length CSV to B3");
+    eprintln!("import pair   CSV to F3");
+    eprintln!("remember to replace cell data!");
 }
 
 fn extract_pair_info(node: ElementRef) -> HashMap<(char, char), usize> {
