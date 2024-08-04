e460a30328d127db7ea1a7afe57aa43f17aa7d9b
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2024-08-04 03:54:48 -0700

    src: handled errors for all except parsing

--- src/main.rs
+++ src/main.rs
@@ -40,26 +40,80 @@ struct Args {
     filename_format: Option<String>,
 }
 
-fn prepare_csv_path(d: &NaiveDate, template: &str, key: &str) -> PathBuf {
-    let csv_name = d.format(&template.replace("_ITEM_", key)).to_string();
-    let csv_path = PathBuf::from(&csv_name);
-    let dirname = csv_path.parent().unwrap();
-    if !dirname.exists() {
-        // TODO: return Result<>
-        std::fs::create_dir_all(dirname).expect("failed to make dir");
-    }
-
-    dirname
-        .canonicalize()
-        .expect("failed to canonicalize path")
-        .join(csv_path.file_name().unwrap())
+#[derive(thiserror::Error, Debug)]
+enum PreparingCSVPathError {
+    #[error("filename template must not end in a slash ({0})")]
+    GivenDirectory(PathBuf),
+    #[error("{0} already exists as a directory")]
+    ExistsAsDirectory(PathBuf),
+    #[error("failed to mkdir {0} ({1})")]
+    Mkdir(PathBuf, std::io::Error),
+    #[error("failed to canonicalise {0} ({1})")]
+    CanonicalisingPath(PathBuf, std::io::Error),
 }
 
-#[tokio::main]
-async fn main() {
+fn prepare_csv_path(
+    d: &NaiveDate,
+    template: &str,
+    key: &str,
+) -> Result<PathBuf, PreparingCSVPathError> {
+    let t = match template {
+        "" => DEFAULT_FORMAT,
+        t => t,
+    };
+    let csv_name = d.format(&t.replace("_ITEM_", key)).to_string();
+    let csv_path = PathBuf::from(&csv_name);
+
+    let dirname = match csv_path.parent() {
+        Some(d) => d,
+        // If somebody really wants to write to /, that's okay I guess.
+        None => return Ok(csv_path),
+    };
+
+    if !dirname.exists() {
+        std::fs::create_dir_all(dirname)
+            .map_err(|e| PreparingCSVPathError::Mkdir(dirname.to_path_buf(), e))?
+    } else if csv_path.is_dir() {
+        return Err(PreparingCSVPathError::ExistsAsDirectory(csv_path));
+    }
+
+    match csv_path.file_name() {
+        Some(tpl_filename) => {
+            let path = dirname
+                .canonicalize()
+                .map_err(|e| PreparingCSVPathError::CanonicalisingPath(dirname.to_path_buf(), e))?
+                .join(tpl_filename);
+
+            Ok(path)
+        }
+        None => Err(PreparingCSVPathError::GivenDirectory(csv_path)),
+    }
+}
+
+#[derive(thiserror::Error, Debug)]
+enum Error {
+    #[error("failed to parse {0} into a date ({1})")]
+    ParsingDate(String, chrono::ParseError),
+    #[error("failed to get info page ({0})")]
+    FetchingUrl(reqwest::Error),
+    #[error("got bad http status from server ({0})")]
+    BadResponse(reqwest::Error),
+    #[error("failed to read response body ({0})")]
+    ReadingBody(reqwest::Error),
+    #[error("error preparing CSV path for {0} ({1})")]
+    PreparingCSVPath(&'static str, PreparingCSVPathError),
+    #[error("error opening ouptut file for {0} ({1}")]
+    OpeningCSVFile(&'static str, csv::Error),
+    #[error("error writing output line for {0} ({1})")]
+    WritingCSVRecord(&'static str, csv::Error),
+}
+
+async fn real_main() -> Result<(), Error> {
     let args = Args::parse();
     let today = match args.date {
-        Some(input_str) => input_str.parse().expect("failed to parse time"),
+        Some(input_str) => input_str
+            .parse()
+            .map_err(|e| Error::ParsingDate(input_str, e))?,
         None => chrono::Utc::now().with_timezone(&US_WEST_TZ).date_naive(),
     };
 
@@ -71,11 +125,11 @@ async fn main() {
     // TODO: subtle user agent?
     let resp = reqwest::get(url_str)
         .await
-        .expect("failed to get url")
+        .map_err(Error::FetchingUrl)?
         .error_for_status()
-        .expect("bad status from resp");
+        .map_err(Error::BadResponse)?;
 
-    let body = resp.text().await.expect("failed to read body");
+    let body = resp.text().await.map_err(Error::ReadingBody)?;
     let page = Html::parse_document(&body);
 
     let table = match page.select(&TABLE_SELECTOR).next() {
@@ -91,10 +145,11 @@ async fn main() {
     let pairs = extract_pair_info(two_letters_el);
     let table_info = extract_table_info(table);
 
-    // NOTE: can't use an unwrap_or here due to lifetime requirements
     let template = args.filename_format.as_deref().unwrap_or(DEFAULT_FORMAT);
-    let lengths_path = prepare_csv_path(&today, template, "lengths");
-    let mut writer = csv::Writer::from_path(&lengths_path).expect("failed to open file");
+    let lengths_path = prepare_csv_path(&today, template, "lengths")
+        .map_err(|e| Error::PreparingCSVPath("lengths", e))?;
+    let mut writer = csv::Writer::from_path(&lengths_path)
+        .map_err(|err| Error::OpeningCSVFile("lengths", err))?;
 
     for ((letter, len), quantity) in table_info.iter() {
         // NOTE: csv writer expects these to be representable as &[u8], even if
@@ -103,16 +158,18 @@ async fn main() {
         let record = [letter.to_string(), len.to_string(), quantity.to_string()];
         writer
             .write_record(&record)
-            .expect("failed to write length record");
+            .map_err(|e| Error::WritingCSVRecord("lengths", e))?;
     }
 
-    let pairs_path = prepare_csv_path(&today, template, "pairs");
-    let mut writer = csv::Writer::from_path(&pairs_path).expect("failed to open pairs file");
+    let pairs_path = prepare_csv_path(&today, template, "pairs")
+        .map_err(|e| Error::PreparingCSVPath("pairs", e))?;
+    let mut writer = csv::Writer::from_path(&pairs_path)
+        .map_err(|error| Error::OpeningCSVFile("pairs", error))?;
     for ((a, b), v) in pairs.iter() {
         let record = [format!("{a}{b}"), v.to_string()];
         writer
             .write_record(record)
-            .expect("failed to write pair record");
+            .map_err(|e| Error::WritingCSVRecord("pairs", e))?;
     }
 
     eprintln!("operation success!");
@@ -125,6 +182,16 @@ async fn main() {
     eprintln!("import length CSV to B3");
     eprintln!("import pair   CSV to F3");
     eprintln!("remember to replace cell data!");
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(e) = real_main().await {
+        eprintln!("error: {e}");
+        std::process::exit(1);
+    }
 }
 
 fn extract_pair_info(node: ElementRef) -> HashMap<(char, char), usize> {
